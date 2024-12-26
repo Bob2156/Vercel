cd58716bbc21197ba1ecf2359c411b139ee9a935
Bob2156 <152450330+Bob2156@users.noreply.github.com>
2024-12-25 20:50:17 -0800

    Update discord.js

--- api/discord.js
+++ api/discord.js
@@ -19,7 +19,7 @@ module.exports = async (req, res) => {
       return res.status(200).json({ type: 1 });
     }
 
-    // Handle application commands
+    // Respond to a sample slash command `/hello`
     if (interaction.type === 2 && interaction.data.name === "hello") {
       return res.status(200).json({
         type: 4,
@@ -33,7 +33,6 @@ module.exports = async (req, res) => {
   res.status(405).send("Method not allowed");
 };
 
-// Helper to get raw request body
 async function getRawBody(req) {
   return new Promise((resolve, reject) => {
     let data = "";
